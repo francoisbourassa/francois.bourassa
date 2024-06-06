e9a633b7c231e80ae258c2af82aee2df5fc95377
Francois Bourassa <118738089+francoisbourassa@users.noreply.github.com>
2024-06-06 15:37:07 -0400

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./assets/css/styles.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
